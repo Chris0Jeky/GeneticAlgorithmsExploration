4c8d046f36261190e602b18772173efc9db4b991
Chris <59696583+Chris0Jeky@users.noreply.github.com>
2024-10-22 01:00:06 +0100

    added constants and outputting them

--- AILabExperimenting/src/Main.java
+++ AILabExperimenting/src/Main.java
@@ -1,5 +1,17 @@
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
+    static final int GENE_LENGTH = 20;
+    static final int POPULATION_SIZE = 10;
+    static final Random RANDOM = new Random();
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        System.out.println("GENE_LENGTH: " + GENE_LENGTH);
+        System.out.println("POPULATION_SIZE: " + POPULATION_SIZE);
+        System.out.println("RANDOM: " + RANDOM.nextInt());
+
     }
 }
